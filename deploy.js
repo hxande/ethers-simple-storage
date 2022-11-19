0115fd796f800e1f8f0c1cbf0360acc957efe409
Hugo <c1297097@bb.com.br>
2022-11-18 22:23:50 -0300

    first commit

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
+async function main() {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    // const encryptedJson = fs.readFileSync("./encryptedKey.json", "utf8");
+    // let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD);
+    // wallet = await wallet.connect(provider);
+    const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi", "utf8");
+    const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin", "utf8");
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+    console.log("Deploying, please wait...");
+    const contract = await contractFactory.deploy();
+    console.log(`Contract address: ${contract.address}`);
+    // const deploymentReceipt = await contract.deployTransaction.wait(1);
+    const currentFavoriteNumber = await contract.retrieve();
+    console.log(currentFavoriteNumber.toString());
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
